81f6b218442b66c5cfc049d3538845290ae8ac43
mehrdad-kj <mehrdadk.jourabi@gmail.com>
2024-12-05 12:45:06 +0330

    Added services section

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import About from "./components/About"
-import Header from "./components/Header"
-import Slider from "./components/Slider"
-
+import About from "./components/About";
+import Header from "./components/Header";
+import Services from "./components/Services";
+import Slider from "./components/Slider";
 
 function App() {
-
   return (
     <div className="min-h-screen bg-gray-100">
       <Header />
       <Slider />
       <About />
+      <Services />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Services.tsx
@@ -0,0 +1,81 @@
+import HTML from "../assets/html.png";
+import CSS from "../assets/css.png";
+import JavaScript from "../assets/javascript.png";
+import React from "../assets/react.png";
+import Tailwind from "../assets/tailwind.png";
+import Git from "../assets/Git.png";
+import Next from "../assets/next.png";
+import Redux from "../assets/redux.png";
+
+type Image = {
+  image: string;
+  alt: string;
+};
+
+const Services = () => {
+  const images: Image[] = [
+    {
+      image: HTML,
+      alt: "HTML",
+    },
+    {
+      image: CSS,
+      alt: "CSS",
+    },
+    {
+      image: JavaScript,
+      alt: "JavaScript",
+    },
+    {
+      image: React,
+      alt: "React",
+    },
+    {
+      image: Tailwind,
+      alt: "Tailwind",
+    },
+    {
+      image: Git,
+      alt: "Git",
+    },
+    {
+      image: Next,
+      alt: "Next",
+    },
+    {
+      image: Redux,
+      alt: "Redux",
+    },
+  ];
+
+  return (
+    <div id="SERVICES" className="w-full h-[70%] bg-sky-950 text-[#C3CEDA] py-10">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full mt-10">
+        <div>
+          <p className="text-4xl font-bold inline-block border-b-4 border-[#ffff2e] ">
+            Services
+          </p>
+          <p className="py-4"> These are the technologies I've worked with</p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+          {images.map((image) => (
+            <div
+              key={image.alt}
+              className="shadow-md shadow-[#040c16] hover:scale-110 transition-all ease-in-out duration-500"
+            >
+              <img
+                className="w-auto h-20 mx-auto my-1 object-cover"
+                src={image.image}
+                alt={image.alt}
+              />
+              <p className="my-4">{image.alt}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
